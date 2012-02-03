551407c2c23d0b78bea245385744374c6e4f8946
Tim Hacker <tim.hacker@trueclarity.co.uk>
2012-02-03 15:19:12 +0000

    added hover javascript

---
+++ KatePhotography/Scripts/hover.js
@@ -0,0 +1,8 @@
+﻿/* File Created: February 3, 2012 */
+
+$(function () {
+	$(".albumLink").click(function () {
+		return $('.albumNavigation').slideToggle('slow');
+	});
+	return $("ul.albumNavigation.current").show();
+});
